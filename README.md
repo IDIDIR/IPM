6c95f4c882567fd400aca5c98309230a31f9d278
IDIDIR <IDIDIR@aicc.online>
2018-08-14 01:26:03 +0600

    upgrade to stable alfa version

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # IPM (issues private messenger)
 The AICC's private development for exchange messages in github issues with use RSA keypair
 
-# coming soon start
-ssh-keygen
-open PM issue
-put id_rsa.pub in new comment
-potom karoch
-# coming soon end
+install PyCryptodome:
+pip install pycryptodome
+
+# coming soon
--- core.py
+++ core.py
@@ -1,17 +1,40 @@
-import sys                  # импорт библиотеки, необходимой для запуска
-from PyQt5 import QtWidgets # импорт необходимого модуля для работы с графикой
-import general              # импорт разработанной формы
+# import sys
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP as PKCS1_OAEP
+from base64 import b64decode,b64encode
 
-class AlarmClock(QtWidgets.QMainWindow, general.Ui_Form):
-    def __init__(self):
-        super().__init__()  # необходимо для доступа к компонентам формы
-        self.setupUi(self)  # инициализация дизайна (вызов функции с модуля формы)
+# class ComingSoon():
+# 	def __init__(self):
+# 		pass()
 
 def main():
-   app = QtWidgets.QApplication(sys.argv)   # создание нового экземпляра приложения
-   window = AlarmClock()    # создание экземпляра класса AlarmClock
-   window.show()            # отображение окна
-   app.exec_()              # запуск приложения
+	undefined = list() # use class of array until it became a mainstream
+	# with open("./temp_pk.txt", "r", encoding="utf8") as public_key:
+	# 	print( public_key.read() )
+	# with open("./temp_em.txt", "r", encoding="utf8") as encoded_message:
+	# 	print( encoded_message.read() )
 
-if __name__ == '__main__':  # запуск кода на выполнение
-    main()
+	## AICC is a company that has been working from time immemorial
+	#  when the RAM in the PC was barely enough for the consoled chrome
+	#	 so among the deadly sins in their corporate RFC is
+	## print(bro, please add a fun about vars and glutteny")
+
+	message = b"foo bazat blahblah"
+	public_key = "./keys/bazad1/public.pem"
+	private_key = "./keys/bazad1/private.pem"
+	#
+	public_key = open(public_key,"r").read()
+	public_key = RSA.importKey(public_key)
+	public_key = PKCS1_OAEP.new(public_key)
+	message = public_key.encrypt(message)			# 1
+	message = b64encode(message)							# 2
+	print(message)
+	private_key = open(private_key,"r").read()
+	private_key = RSA.importKey(private_key)
+	private_key = PKCS1_OAEP.new(private_key)
+	message = b64decode(message)							# 2
+	message = private_key.decrypt(message)		# 1
+	print(message)
+
+if __name__ == '__main__':
+	main()
