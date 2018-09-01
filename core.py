8523dc440df015593be067c6e02feec8b7903aee
IDIDIR <IDIDIR@aicc.online>
2018-09-02 04:02:05 +0600

    added some code lines

--- core.py
+++ core.py
@@ -1,26 +1,41 @@
 # import sys
+import os.path
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP as PKCS1_OAEP
 from base64 import b64decode,b64encode
+
+## AICC is a company that has been working from time immemorial
+#  when the RAM in the PC was barely enough for the consoled chrome
+#  so among the deadly sins in their corporate RFC is
+## print(bro, please add a fun about vars and glutteny")
 
 # class ComingSoon():
 # 	def __init__(self):
 # 		pass()
 
-def main():
-	undefined = list() # use class of array until it became a mainstream
+# undefined = list() # use class of array until it became a mainstream
+
+def selfkey_exist():
+	private_file 	= './keys/__self__/private.pem'
+	return os.path.isfile(private_file)
+
+def selfkey_generate():
+	public_file 	= './keys/__self__/public.pem'
+	private_file 	= './keys/__self__/private.pem'
+	key = RSA.generate(2048)
+	private_key = key.export_key()
+	open(private_file, "wb").write(private_key)
+	public_key = key.publickey().export_key()
+	open(public_file, "wb").write(public_key)
 	
-	# with open("./temp_pk.txt", "r", encoding="utf8") as public_key:
-	# 	print( public_key.read() )
-	# with open("./temp_em.txt", "r", encoding="utf8") as encoded_message:
-	# 	print( encoded_message.read() )
+def main():
+	if (selfkey_exist()):
+		temp()
+	else:
+		print('Selfkeys don\'t found.\nGenerating.....')
+		selfkey_generate()
 
-	## AICC is a company that has been working from time immemorial
-	#  when the RAM in the PC was barely enough for the consoled chrome
-	#  so among the deadly sins in their corporate RFC is
-	## print(bro, please add a fun about vars and glutteny")
-
-
+def temp():
 	# send public_2 over GitHub (recipient)
 	public_file = "./keys/bazad2/public.pem"
 	public_key = open(public_file,"r").read()
