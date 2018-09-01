71a99d722d5a2f73646e6076cefe4d17fa3b5b4b
IDIDIR <IDIDIR@aicc.online>
2018-09-02 01:43:45 +0600

    don't touch when it's work

--- core.py
+++ core.py
@@ -9,6 +9,7 @@ from base64 import b64decode,b64encode
 
 def main():
 	undefined = list() # use class of array until it became a mainstream
+	
 	# with open("./temp_pk.txt", "r", encoding="utf8") as public_key:
 	# 	print( public_key.read() )
 	# with open("./temp_em.txt", "r", encoding="utf8") as encoded_message:
@@ -19,22 +20,32 @@ def main():
 	#  so among the deadly sins in their corporate RFC is
 	## print(bro, please add a fun about vars and glutteny")
 
-	message = b"foo bazat blahblah"
-	public_key = "./keys/bazad1/public.pem"
-	private_key = "./keys/bazad1/private.pem"
-	#
-	public_key = open(public_key,"r").read()
+
+	# send public_2 over GitHub (recipient)
+	public_file = "./keys/bazad2/public.pem"
+	public_key = open(public_file,"r").read()
+	tag_KEYS = "./temp_pk.txt"
+	open(tag_KEYS,"w").write(public_key)
+	# encrypt message with public_2 (sender)
+	public_file = "./temp_pk.txt"
+	public_key = open(public_file,"r").read()
 	public_key = RSA.importKey(public_key)
 	public_key = PKCS1_OAEP.new(public_key)
+	message = "foo bazat blahblah"
+	message = message.encode('utf-8')
 	message = public_key.encrypt(message)			# 1
-	message = b64encode(message)				# 2
-	print(message)
-	private_key = open(private_key,"r").read()
-	private_key = RSA.importKey(private_key)
+	message = b64encode(message)							# 2
+	tag_PM = "./temp_em.txt"
+	open(tag_PM,"w").write(message.decode('utf-8'))
+	# decrypt message with private_2 (recipient)
+	private_key = "./keys/bazad2/private.pem"
+	private_file = open(private_key,"r").read()
+	private_key = RSA.importKey(private_file)
 	private_key = PKCS1_OAEP.new(private_key)
-	message = b64decode(message)				# 2
-	message = private_key.decrypt(message)			# 1
-	print(message)
+	message = b64decode(message)							# 2
+	message = private_key.decrypt(message)		# 1
+	print(message.decode('utf-8'))
+
 
 if __name__ == '__main__':
 	main()
